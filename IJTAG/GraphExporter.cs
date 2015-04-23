d7eab8b1f46ac18b2ba094de79a6a2fe2d53b497
Mehrdad Montazeri <mehrdad.montazeri@comtestwireless.it>
2015-04-23 12:21:32 +0200

    init

---
+++ IJTAG/GraphExporter.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IJTAG
+{
+    public static class extentionStack
+    {
+        public static Stack<T> Clone<T>(this Stack<T> stack)
+        {
+            return new Stack<T>(new Stack<T>(stack));
+        }
+    }
+
+    public class SIB
+    {
+        public static int StringToInt(string s)
+        {
+            int d = 0;
+            int.TryParse(s, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out d);
+            return d;
+        }
+
+        public static uint StringToUInt(string s)
+        {
+            uint d = 0;
+            uint.TryParse(s, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out d);
+            return d;
+        }
+
+        public override string ToString()
+        {
+            return "ID = " + ID + "(" + Level + "," + Column + ")" + Size;
+        }
+
+        public SIB(XElement Element, SIB parent)
+        {
+            this.Parent = parent;
+            if (parent != null)
+            {
+                parent.Children.Add(this);
+            }
+            this.Type = Element.Name.LocalName;
+
+            foreach (XAttribute att in Element.Attributes())
+            {
+                switch (att.Name.LocalName)
+                {
+                    case "ID":
+                        ID = att.Value;
+                        break;
+                    case "InstrumentID":
+                        InstrumentID = att.Value;
+                        break;
+                    case "SCPatterns":
+                        SCPattherns = StringToInt(att.Value);
+                        break;
+                    case "SCLength":
+                        SCLength = StringToUInt(att.Value);
+                        break;
+                    case "WIRLength":
+                        WIRLength = StringToInt(att.Value);
+                        break;
+
+                }
+            }
+        }
+
+        public SIB()
+        {
+            // Fake
+        }
+
+        //charactersitics
+        public readonly string Type;
+        public readonly string InstrumentID;
+        public readonly string ID;
+        public readonly int SCPattherns;
+        public readonly uint SCLength;
+        public readonly int WIRLength;
+        public readonly XElement RelatedElem;
+        
+        //Pattern Tested
+        public bool neverCheckable { get { return ID == null || Parent.Children.Last() == this; } }
+        public bool IsBypassOnly { get { return Children.Count == 0 && SCLength == 0; } }
+        public int CheckedOpen = 0;
+        public int CheckedClose = 0;
+        public bool IsFullyChecked
+        {
+            get
+            {
+                //if (IsBypassOnly)
+                //{
+                //    return CheckedClose > 0;
+                //}
+                //else
+                {
+                    return CheckedClose > 0 && CheckedOpen > 0;
+                }
+            }
+        }
+        //Relations
+        public readonly List<SIB> Children = new List<SIB>();
+        public readonly SIB Parent;
+        public SIB source;
+        //public SIB dest;
+        
+        #region For Drawing 
+        public int Level
+        {
+            get
+            {
+                int i = 0;
+                SIB temp = this;
+                while (temp != null)
+                {
+                    temp = temp.Parent;
+                    i++;
+                }
+                return i;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return Math.Max(1, Children.Sum(x => x.Size));
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                if (Parent != null)
+                {
+                    int myindex = Parent.Children.IndexOf(this);
+                    int adding = 0;
+                    if (myindex != 0)
+                        adding = Parent.Children.Take(myindex).Sum(x => x.Size);
+                    return Parent.Column + adding;
+                }
+                else
+                    return 0;
+            }
+        }
+
+
+        #endregion
+
+
+
+        //internal SIB Clone()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+    public class GraphExporter
+    {
+        SIB gateway;
+
+        List<SIB> AllSIBs = new List<SIB>();
+
+        List<Stack<SIB>> AllPaths = new List<Stack<SIB>>();
+        public List<Tuple<Stack<SIB>, uint>> outputPaths = new List<Tuple<Stack<SIB>, uint>>();
+
+        public long sumofLenght;
+        public long ConfigLenght;
+
+        public int Dept { get { return AllSIBs.Max(x => x.Level); } }
+
+        public GraphExporter()
+        {
+        }
+
+        public void Parse(XElement root)
+        {
+            gateway = new SIB(root, null);
+            AllSIBs.Add(gateway);
+            ParallelConstruction(root, gateway);
+
+            SIB TDILast = AllSIBs.Where(x => x.Level == 2).Last();
+            TDIFirst = AllSIBs.Where(x => x.Level == 2).First();
+            Stack<SIB> first = new Stack<SIB>();
+            AllPaths.Add(first);
+            RecursivePaterns(TDILast, first);
+
+
+            AllPaths = AllPaths.OrderByDescending(x => x.Count).ToList();
+
+
+            outputPaths.Clear();
+
+            foreach (Stack<SIB> path in AllPaths)
+            {
+                if (AllSIBs.Where(x => x.neverCheckable == false).All(x => x.IsFullyChecked))
+                {
+                    break;
+                }
+
+                outputPaths.Add(new Tuple<Stack<SIB>, uint>(path, ControlPath(path)));
+            }
+
+            
+            //var totalPathLength = AllPaths.Select(y => new Tuple<Stack<SIB>, int>(y, y.Sum(s => s.SCLength)));//.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+
+            //var SibstoCheck = AllSIBs.Select(x => new Tuple<SIB, bool, bool>(x, false, false));
+
+            /*outputPaths.Clear();
+            ControlAllPaths();
+
+            sumofLenght = outputPaths.Sum(x => x.Sum(s => s.SCLength));
+            ConfigLenght = outputPaths.Sum(x => x.Sum(s => s.SCLength) * x.Max(l => l.Level));
+            */
+              //outputPaths.
+
+            //var t = AllSIBs.Where(x => x.neverCheckable == false).Where(x => x.IsFullyChecked == false);
+
+            /*
+            foreach (var pat in AllPaths)
+            {
+                var Appliedpath = ApplyPath(pat);
+
+                foreach (var ap in Appliedpath)
+                {
+                    //if (ap.Item2
+                }
+            }
+
+            Dictionary<SIB, Tuple<bool, bool>> SibsControl = new Dictionary<SIB, Tuple<bool, bool>>();
+            Dictionary<Stack<SIB>, List<Tuple<SIB, bool, bool>>> Path_coverage = new Dictionary<Stack<SIB>, List<Tuple<SIB, bool, bool>>>();
+            foreach (var pat in AllPaths)
+            {
+                var Appliedpath = ApplyPath(pat);
+                Path_coverage.Add(pat, Appliedpath);
+
+                foreach (var Ap in Appliedpath)
+                {
+                    if (SibsControl.ContainsKey(Ap.Item1) == false)
+                    {
+                        SibsControl.Add(Ap.Item1, new Tuple<bool, bool>(Ap.Item2, Ap.Item3));
+                    }
+                    else
+                    {
+                        //if (SibsControl[Ap.Item1].Item1 == false &&
+
+                    }
+                }
+            }
+             * */
+        }
+
+
+        uint ControlPath(Stack<SIB> path)
+        {
+            uint Len = 0;
+
+            var clon = path.Clone();
+
+            while (clon.Count > 1)
+            {
+                SIB me = clon.Pop();
+
+                bool isClose;
+
+                if (me.Children.Count > 0 && me.Children.First() == clon.Peek())
+                {
+                    isClose = false;
+                    //me.CheckedOpen++;
+                }
+                else
+                {
+                    isClose = true;
+                    //me.CheckedClose++;
+                }
+
+                if (isClose && me.Children.Count == 0)
+                {
+                    if (me.CheckedOpen == 0)
+                    {
+                        isClose = false;
+                    }
+                }
+
+                if (isClose)
+                {
+                    me.CheckedClose++;
+                    Len += 1;
+                }
+                else
+                {
+                    me.CheckedOpen++;
+                    Len += 1 + me.SCLength;
+                }
+            }
+            return Len;
+        }
+
+        //         SIB,Closed,Opened
+        List<Tuple<SIB, bool, bool>> ApplyPath(Stack<SIB> path)
+        {
+            List<Tuple<SIB, bool, bool>> list = new List<Tuple<SIB, bool, bool>>();
+
+            var clon = path.Clone();
+            while (clon.Count > 1)
+            {
+                SIB me = clon.Pop();
+                if (me.IsBypassOnly)
+                {
+                    list.Add(new Tuple<SIB, bool, bool>(me, true, true));
+                }
+                else
+                {
+                    if (me.Children.Count > 0 && me.Children.First() == clon.Peek())
+                    {
+                        list.Add(new Tuple<SIB, bool, bool>(me, true, false));
+                    }
+                    else
+                    {
+                        list.Add(new Tuple<SIB, bool, bool>(me, false, true));
+                    }
+                }
+            }
+            return list;
+        }
+
+        private void ControlAllPaths()
+        {
+            foreach (Stack<SIB> path in AllPaths)
+            {
+                if (AllSIBs.Where(x => x.neverCheckable == false).All(x => x.IsFullyChecked))
+                {
+                    break;
+                }
+
+                //outputPaths.Add(path);
+
+                var clon = path.Clone();
+                
+                while (clon.Count > 1)
+                {
+                    SIB me = clon.Pop();
+                    if (me.Children.Count > 0 && me.Children.First() == clon.Peek())
+                    {
+                        me.CheckedOpen++;
+                    }
+                    else
+                    {
+                        me.CheckedClose++;
+                    }
+                }
+            }
+        }
+
+        SIB TDIFirst;
+
+        
+
+        void RecursivePaterns(SIB Node, Stack<SIB> path)
+        {
+            path.Push(Node);
+            if (Node != TDIFirst)
+            {
+                if (Node.Children.Count == 0)
+                {
+                    if (Node.source != null)
+                    {
+                        RecursivePaterns(Node.source, path);
+                    }
+                }
+                else
+                {
+                    if (path.Contains(Node.Children.Last()) == false)
+                    {
+                        var copy = extentionStack.Clone(path);
+                        AllPaths.Add(copy);
+                        RecursivePaterns(Node.Children.Last(), copy);
+                        if (Node.source != null)
+                        {
+                            RecursivePaterns(Node.source, path);
+                        }
+                    }
+                    else
+                    {
+                        if (Node.source != null)
+                        {
+                            RecursivePaterns(Node.source, path);
+                        }
+                    }
+                }
+
+                if (Node.Parent.Children.First() == Node)
+                {
+                    RecursivePaterns(Node.Parent, path);
+                }
+            }
+        }
+
+        public void ParallelConstruction(XElement root, SIB parent)
+        {
+            var childs = root.Elements("SIB");
+            if (childs.Count() > 0)
+            {
+                var e = childs.GetEnumerator();
+                e.MoveNext();
+                var last = new SIB(e.Current, parent);
+                AllSIBs.Add(last);
+                ParallelConstruction(e.Current, last);
+
+                while (e.MoveNext())
+                {
+                    SIB me = new SIB(e.Current, parent);
+                    AllSIBs.Add(me);
+                    me.source = last;
+                    ParallelConstruction(e.Current, me);
+                    last = me;
+                }
+            }
+        }
+
+        internal List<Stack<SIB>> getAllPaths() 
+        {
+            return AllPaths.OrderByDescending(x => x.Count).ToList();
+        }
+
+        internal void Dispose()
+        {
+        }
+
+        internal List<SIB> GetAllNodes()
+        {
+            return AllSIBs;//.Clone();
+        }
+        
+        internal SIB GetGateway()
+        {
+            return gateway;//.Clone();
+        }
+    }
+}
---
+++ IJTAG/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IJTAG
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GraphExporter exporter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fl = new System.Windows.Forms.FolderBrowserDialog();
+            if (fl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamWriter tx = System.IO.File.CreateText(fl.SelectedPath + "\\log.csv");
+                tx.WriteLine("FileName;Depth of nodes;All Paths;Paths to test;length of full test;length of configuration");
+                foreach (var file in System.IO.Directory.GetFiles(fl.SelectedPath, "*.xml", System.IO.SearchOption.AllDirectories))
+                {
+                    exporter = new GraphExporter();
+
+                    System.IO.StreamReader read = new System.IO.StreamReader(file);
+                    System.Xml.Linq.XDocument doc = System.Xml.Linq.XDocument.Load(read);
+                    exporter.Parse(doc.Element("Gateway"));
+
+                    tx.WriteLine(System.IO.Path.GetFileNameWithoutExtension(file) + ";" + exporter.Dept + ";" + exporter.getAllPaths().Count + ";" + exporter.outputPaths.Count + ";" + exporter.sumofLenght + ";" + exporter.ConfigLenght);
+
+                }
+                tx.Close();
+                tx.Dispose();
+
+                MessageBox.Show("finished: " + fl.SelectedPath + "\\log.csv");
+
+            }
+        }
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            exporter = new GraphExporter();
+            System.Windows.Forms.OpenFileDialog fl = new System.Windows.Forms.OpenFileDialog();
+            fl.Filter = "XML File (*.Xml)|";
+            fl.Multiselect = false;
+
+            if (fl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //try
+                {
+                    System.IO.StreamReader read = new System.IO.StreamReader(fl.FileName);
+                    System.Xml.Linq.XDocument doc = System.Xml.Linq.XDocument.Load(read);
+                    exporter.Parse(doc.Element("Gateway"));
+
+                    Console.WriteLine(fl.FileName + "had " + exporter.getAllPaths().Count + " Paths with lenght of " + exporter.sumofLenght + " in " + exporter.outputPaths.Count); 
+                }
+                //catch
+                //{
+                //}
+            }
+
+            var AllPathes = exporter.getAllPaths();
+            PathsCount.Text = AllPathes.Count.ToString();
+
+            foreach (var pat in AllPathes)
+            {
+                combo.Items.Add(new ComboBoxItem() { Content = pat.Count, Tag = pat });
+            }
+
+            BuildCanvas();
+        }
+
+        int XRectSize = 26;
+        int distXrect = 16;
+
+        void BuildCanvas()
+        {
+            Canvas.Children.Clear();
+            Canvas.RowDefinitions.Clear();
+            Canvas.ColumnDefinitions.Clear();
+
+            var Allsibs = exporter.GetAllNodes();
+            SIB gateway = exporter.GetGateway();
+            int MaxLevel = Allsibs.Max(x => x.Level);
+
+            for (int i = 0; i < gateway.Size; i++)
+            {
+                Canvas.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Auto) });
+            }
+
+            for (int i = 0; i < MaxLevel; i++)
+            {
+                Canvas.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+            }
+
+            for (int i = 1; i <= MaxLevel; i++)
+            {
+                var thisLevel = Allsibs.FindAll(x => x.Level == i);
+                for (int j = 0; j < thisLevel.Count; j++)
+                {
+                    SIB sib = thisLevel[j];
+
+                    #region tooltip
+                    Grid tooltipgrid = new Grid();
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Auto) });
+                    tooltipgrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Auto) });
+
+                    TextBlock tb1 = new TextBlock() { Text = sib.Type + " " + sib.ID, FontWeight = FontWeights.Bold };
+                    Grid.SetColumn(tb1, 0); Grid.SetRow(tb1, 0); Grid.SetColumnSpan(tb1, 2); tooltipgrid.Children.Add(tb1);
+                    TextBlock tb2 = new TextBlock() { Text = "SC Length:  " };
+                    Grid.SetColumn(tb2, 0); Grid.SetRow(tb2, 1); tooltipgrid.Children.Add(tb2);
+                    TextBlock tb3 = new TextBlock() { Text = sib.SCLength.ToString() };
+                    Grid.SetColumn(tb3, 1); Grid.SetRow(tb3, 1); tooltipgrid.Children.Add(tb3);
+                    TextBlock tb4 = new TextBlock() { Text = "With All Paths Open Config checked: "  };
+                    Grid.SetColumn(tb4, 0); Grid.SetRow(tb4, 2); tooltipgrid.Children.Add(tb4);
+                    TextBlock tb5 = new TextBlock() { Text = sib.CheckedOpen.ToString() };
+                    Grid.SetColumn(tb5, 1); Grid.SetRow(tb5, 2); tooltipgrid.Children.Add(tb5);
+                    TextBlock tb6 = new TextBlock() { Text = "With All Paths Close Config checked: " };
+                    Grid.SetColumn(tb6, 0); Grid.SetRow(tb6, 3); tooltipgrid.Children.Add(tb6);
+                    TextBlock tb7 = new TextBlock() { Text = sib.CheckedClose.ToString() };
+                    Grid.SetColumn(tb7, 1); Grid.SetRow(tb7, 3); tooltipgrid.Children.Add(tb7);
+                    #endregion
+
+                    Border brd = new Border() { MinWidth = 16, ToolTip = tooltipgrid, Background = Brushes.White, BorderBrush = Brushes.Black, BorderThickness = new Thickness(2), Margin = new Thickness(distXrect), HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch, VerticalAlignment = System.Windows.VerticalAlignment.Center };
+                    allborders.Add(sib, brd);
+                    TextBlock txt = new TextBlock() { Text = sib.ID, TextWrapping = TextWrapping.Wrap, HorizontalAlignment = System.Windows.HorizontalAlignment.Center, Margin = new Thickness(2) };
+                    if (sib.ID == null)
+                    {
+                        txt.Text = "Gateway";
+                    }
+                    brd.Child = txt;
+                    Grid.SetColumn(brd, sib.Column);
+                    Grid.SetRow(brd, sib.Level - 1);
+                    Grid.SetColumnSpan(brd, sib.Size);
+                    Canvas.Children.Add(brd);
+
+                    if (i != 1 && (sib.Parent.Children.First() == sib || sib.Parent.Children.Last() == sib))
+                    {
+                        int uniqeChild = 0;
+                        if (sib.Parent.Children.Count == 1)
+                        {
+                            uniqeChild = 7;
+                            Path arrow = new Path() { Stroke = Brushes.Black, StrokeThickness = 1, FlowDirection = FlowDirection.LeftToRight, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
+                            GeometryGroup geomGroup = new GeometryGroup();
+                            LineGeometry line1 = new LineGeometry() { StartPoint = new Point(4 - uniqeChild, 15), EndPoint = new Point(0 - uniqeChild, 20) };
+                            LineGeometry line2 = new LineGeometry() { StartPoint = new Point(0 - uniqeChild, 0), EndPoint = new Point(0 - uniqeChild, 20) };
+                            LineGeometry line3 = new LineGeometry() { StartPoint = new Point(-4 - uniqeChild, 15), EndPoint = new Point(0 - uniqeChild, 20) };
+                            geomGroup.Children.Add(line1);
+                            geomGroup.Children.Add(line2);
+                            geomGroup.Children.Add(line3);
+                            arrow.Data = geomGroup;
+
+                            Grid.SetColumn(arrow, sib.Column);
+                            Grid.SetRow(arrow, sib.Level - 2);
+                            Grid.SetRowSpan(arrow, 2);
+                            Grid.SetColumnSpan(arrow, sib.Size);
+                            Canvas.Children.Add(arrow);
+                        }
+
+                        {
+                            bool up = sib.Parent.Children.Last() == sib;
+                            Path arrow = new Path() { Stroke = Brushes.Black, StrokeThickness = 1, FlowDirection = FlowDirection.LeftToRight, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment =  HorizontalAlignment.Center };
+                            GeometryGroup geomGroup = new GeometryGroup();
+                            LineGeometry line1 = new LineGeometry() { StartPoint = new Point(4 + uniqeChild, up ? 5 : 15), EndPoint = new Point(0 + uniqeChild, up ? 0 : 20) };
+                            LineGeometry line2 = new LineGeometry() { StartPoint = new Point(0 + uniqeChild, 0), EndPoint = new Point(0 + uniqeChild, 20) };
+                            LineGeometry line3 = new LineGeometry() { StartPoint = new Point(-4 + uniqeChild, up ? 5 : 15), EndPoint = new Point(0 + uniqeChild, up ? 0 : 20) };
+                            geomGroup.Children.Add(line1);
+                            geomGroup.Children.Add(line2);
+                            geomGroup.Children.Add(line3);
+                            arrow.Data = geomGroup;
+
+                            Grid.SetColumn(arrow, sib.Column);
+                            Grid.SetRow(arrow, sib.Level - 2);
+                            Grid.SetRowSpan(arrow, 2);
+                            Grid.SetColumnSpan(arrow, sib.Size);
+                            Canvas.Children.Add(arrow);
+                        }
+                    }
+
+                    if (j != thisLevel.Count - 1)
+                    {
+                        if (thisLevel[j].Parent == thisLevel[j + 1].Parent)
+                        {
+                            Path arrow = new Path() { Stroke = Brushes.Black, StrokeThickness = 1, FlowDirection = FlowDirection.LeftToRight, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center};
+                            GeometryGroup geomGroup = new GeometryGroup();
+                            LineGeometry line1 = new LineGeometry() { StartPoint = new Point(13, 7), EndPoint = new Point(20, 10) };
+                            LineGeometry line2 = new LineGeometry() { StartPoint = new Point(0, 10), EndPoint = new Point(20, 10) };
+                            LineGeometry line3 = new LineGeometry() { StartPoint = new Point(13, 13), EndPoint = new Point(20, 10) };
+                            geomGroup.Children.Add(line1);
+                            geomGroup.Children.Add(line2);
+                            geomGroup.Children.Add(line3);
+                            arrow.Data = geomGroup;
+
+                            Grid.SetRow(arrow, sib.Level - 1);
+                            Grid.SetColumn(arrow, sib.Column + sib.Size - 1);
+                            Grid.SetColumnSpan(arrow, 2);
+                            Grid.SetZIndex(arrow, 0);
+                            Canvas.Children.Add(arrow);
+                        }
+                    }
+                }
+            }
+        }
+
+        Dictionary<SIB, Border> allborders = new Dictionary<SIB, Border>();
+
+        private void Paths_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var brd in allborders.Values)
+            {
+                brd.BorderBrush = Brushes.Black;
+            }
+
+            foreach (SIB sib in ((combo.SelectedItem as ComboBoxItem).Tag as Stack<SIB>))
+            {
+                allborders[sib].BorderBrush = Brushes.Red;
+            }
+
+        }
+
+        
+    }
+}
